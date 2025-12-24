f03a681e853ea29f357eddc2a3eedb171ea851f7
Daniel <nakajito@proton.me>
2025-12-24 18:47:46 -0500

    submit darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,36 +1,17 @@
+from math import sqrt
+
+
 def score(x, y):
     points = 0
 
-    # Missed target: 0 points
-    if (x <= -9) and (y >= 9):
-        points = 0
+    if sqrt(x**2 + y**2) <= 1:
+        points = 10
         return points
-
-    # On the outer circle
-    if x == 0 and y == 10:
-        points = 1
-        return points
-
-    # On the middle circle
-    if x == -5 and y == 0:
+    elif sqrt(x**2 + y**2) <= 5:
         points = 5
         return points
-
-    # On the inner circle
-    if x == 0 and y == -1:
-        points = 10
+    elif sqrt(x**2 + y**2) <= 10:
+        points = 1
         return points
-    
-    # Exactly on center: 10 points
-    if x == 0 and y == 0:
-        points = 10
+    else:
         return points
-
-    # Near the center
-    if (x > -5 and x <= 5 ) and (y >= -5 and y <= 5):
-        points = 10
-        return points
-
-        
-        
-    
